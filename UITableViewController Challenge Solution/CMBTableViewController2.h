3cb7ec3f7b920aa57d46b58264cd30b6a5524cae
ChristineBuell <christine.buell@gmail.com>
2014-04-23 11:25:40 -0400

    Finished assignment

--- UITableViewController Challenge Solution/CMBTableViewController2.h
+++ UITableViewController Challenge Solution/CMBTableViewController2.h
@@ -1,13 +1,15 @@
 //
-//  CMBViewController.h
+//  CMBTableViewController2.h
 //  UITableViewController Challenge Solution
 //
-//  Created by Christine Buell on 4/22/14.
+//  Created by Christine Buell on 4/23/14.
 //  Copyright (c) 2014 Christine Buell. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface CMBViewController : UIViewController
+@interface CMBTableViewController2 : UITableViewController
+
+
 
 @end
